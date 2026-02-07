cae6910cbb08bcf9cd4b62abe6666c2229d6a0ae
Hasan Hasim <hasim30042005@gmail.com>
2026-02-07 18:27:39 +0530

    changes

--- 5-2-26/pattern1.java
+++ 5-2-26/pattern1.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-
 class pattern1
 {
     public static void main(String args[])
--- 5-2-26/pattern2.java
+++ 5-2-26/pattern2.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-
 public class pattern2 {
     public static void main(String args[])
     {   
