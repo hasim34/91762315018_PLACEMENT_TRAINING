65cb4be4fe305fe4ec2f939e343de5a119290b08
Hasan Hasim <hasim30042005@gmail.com>
2026-02-08 19:20:04 +0530

    pattern22

--- 5-2-26/pattern22.java
+++ 5-2-26/pattern22.java
@@ -6,7 +6,19 @@ public class pattern22 {
         System.out.println("Enter the number : ");
         int n = sc.nextInt();
         int i, j;
-
+        int size = 2*n-1;
+        
+        for(i=1; i<=size; i++){
+            for(j=1; j<=size; j++){
+                int top = i-1;
+                int left = j-1;
+                int right = size-j;
+                int bottom = size-i;
+                int min = Math.min(Math.min(top, bottom), Math.min(left, right));
+                System.out.print(n-min);
+            }
+            System.out.println();
+        }
         
     }
 }
